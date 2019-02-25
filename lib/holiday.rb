134afbd69b3ab1ff9454ac3993eb7dbc9c4c3b3a
Levik Naparstek <levik010@gmail.com>
2019-02-25 00:43:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, item|
       holiday if item.include?("BBQ")
-      holiday_hash.flatten
+      holiday_hash.flatten.compact
     end 
   end
 end
